f70d40a3b33de3e5ef44d5c6a9dc79ea93698f38
Arman Sharif <armandino@gmail.com>
2011-11-02 20:03:42 -0700

    Refactored pie.py.

--- pie.py
+++ pie.py
@@ -1,156 +1,143 @@
-#!/usr/bin/env python
+import logging
+import urwid
+import subprocess
 
 import argparse
 import re
 import os
 from os.path import isfile
-import subprocess
 import sys
 
-EXCLUDED_DIRS=['.hg', 'target', 'bin', 'classes']
 
-# TODO: EXCLUDED_FILE_EXT=['.class', '.jar']
+class PieUI(urwid.ListWalker):
 
-def listFiles(baseDir, searchstring, results=[]):
-    dirList = os.listdir(baseDir)
-    subDirs = []
-    for item in dirList:
-        path = os.path.join(baseDir, item)
-        if isfile(path):
-            if matches(searchstring, item):
-                 # remove ./ at the beginning (can do with os.path?)
-                results.append(path[2:])
-        else:
-            if item not in EXCLUDED_DIRS:
-                subDirs.append(path)
+    logging.basicConfig(filename='/tmp/pie.log', level=logging.DEBUG)
+    log = logging.getLogger()
+    
+    def __init__(self, path_list):
+        self.items = []
+        for path in path_list:
+            self.items.append(urwid.Text(path))
+        
+        content = urwid.SimpleListWalker([
+                urwid.AttrMap(w, None, 'reveal focus') for w in self.items])
+        
+        self.listbox = urwid.ListBox(content)
+        
+        self.header_text = urwid.Text("Press any key", wrap='clip')
+        footer_text = urwid.Text("", wrap='clip')
+        footer_text.set_text("[o]pen, [d]iff, [q]uit")
+        
+        head = urwid.AttrMap(self.header_text, 'header')
+        foot = urwid.AttrMap(footer_text, 'footer')
+        top = urwid.Frame(self.listbox, head, foot)
+        
+        palette = [
+            ('header', 'white', 'black'),
+            ('footer', 'white', 'dark green'),
+            ('reveal focus', 'black', 'dark cyan', 'standout')]
+        
+        self.loop = urwid.MainLoop(
+            top, palette,
+            input_filter=self.input_handler)
 
-    for subDir in subDirs:
-        listFiles(subDir, searchstring, results)
+    # these actions should be factored out of UI
+    def open_file(self, path, **kwargs):
+        editor_cmd = kwargs['editor']
+        cmd = editor_cmd.split()
+        cmd.append(path)
+        self.log.debug(cmd)
+        subprocess.call(cmd)
+        
+    def svn_diff(self, path, **kwargs):
+        self.log.debug("svn diff " + path)
+        subprocess.call("svn diff " + path + " | less", shell=True)
 
-    return results
+    def get_focus_index(self):
+        focus_widget, idx = self.listbox.get_focus()
+        return idx
 
-def matches(searchstring, item):
-    return re.search(searchstring, item, re.IGNORECASE)
+    def get_focus_path(self):
+        idx = self.get_focus_index()
+        path, attrs = self.items[idx].get_text()
+        return path
 
-def excludedDir(dir):
-    excluded = dir in EXCLUDED_DIRS
-    return excluded
+    def input_handler(self, input, raw):
+        key = " ".join([unicode(i) for i in input])
+        self.header_text.set_text("Pressed: " + key)
+        
+        if key == 'down':
+            self.log.debug('down')
+            idx = self.get_focus_index()
+            self.log.debug('idx='+str(idx))
+            
+            if idx < len(self.items) - 1:
+                idx = idx+1
+                self.listbox.set_focus(idx)
+        elif key == 'up':
+            idx = self.get_focus_index()
+            self.log.debug('idx='+str(idx))
+            if idx > 0:
+                idx = idx-1
+                self.listbox.set_focus(idx)
+                
+        elif key in ('o', 'O'):
+            path = self.get_focus_path()
+            self.open_file(path, editor='emacs -nw')
+        elif key in ('d', 'D'):
+            path = self.get_focus_path()
+            self.svn_diff(path)
+        elif key in ('q', 'Q'):
+            raise urwid.ExitMainLoop()
 
-
-""" Note: sub-process cannot change parent shell's CWD,
-so the best we can do is just print the command for copy/pasting.
-"""
-def cd(path, **kwargs):
-    print "cd", path[:path.rfind('/')]
-
-def svn_diff(path, **kwargs):
-    print "svn diff", path
-    subprocess.call(["svn", "diff", path])
-
-def hg_diff(path, **kwargs):
-    print "hg diff", path
-    subprocess.call(["hg", "diff", path])
-
-def open_file(path, **kwargs):
-    open_cmd = kwargs['editor']
-    print open_cmd, path
-    subprocess.call([open_cmd, path])
-
-def execute(dir, searchstring, action, index, **kwargs):
-    results = listFiles(os.curdir, searchstring)    
-    if index is None and action is None:
-        outputResults(results)
-    else:
-        checkIndex(index, results)
-        if action is None: action = open_file
-        executeAction(action, index, results, **kwargs)
-
-def executeAction(action, index, results, **kwargs):
-    for i, item in enumerate(results):
-        if i == index: action(item, **kwargs)
-
-def outputResults(results):
-    for i, path in enumerate(results): print "%i: %s" % (i, path)
-
-def checkIndex(index, results):
-    if index < 0 or index > len(results) - 1:
-        raise ValueError("There is no result number [%i]" % index)
-
-def getIndex(args, action):
-    if args.index is not None:
-        return args.index
-    elif action:
-        return 0 # by default action on item 0
-    return None # index won't be used
-
-def getOpenCmd(args):
-    if args.open is None:
-        # default to EDITOR
-        return os.environ.get('EDITOR')
-    else:
-        return args.open
-
-
-def getAction(args):
-    if args.cd:
-        return cd
-    elif args.svn_diff:
-        return svn_diff
-    elif args.hg_diff:
-        return hg_diff
-    elif args.open:
-        return open_file
-    return None
+    def start(self):
+        self.loop.run()
 
 
-#eclipse --launcher.openFile <absolute path of file to open>
+class PieFind:
+    EXCLUDED_DIRS=['.git', '.hg', 'target', 'bin', 'classes']
 
-# TODO: symlinks seem to be broken
+    def find_files(self, baseDir, searchstring, results=[]):
+        try:
+            dirList = os.listdir(baseDir)
+        except OSError:
+            dirList = []
 
-# find and print results
-# ff file
+        subDirs = []
+        for item in dirList:
+            path = os.path.join(baseDir, item)
+            if isfile(path):
+                if self.matches(searchstring, item):
+                    if path.startswith('./'):
+                        path = path[2:]
+                    results.append(path)
+            else:
+                if item not in self.EXCLUDED_DIRS:
+                    subDirs.append(path)
 
-# find and open file with [num]
-# ff file [num]
+        for subDir in subDirs:
+            self.find_files(subDir, searchstring, results)
 
-# find and open file with [num] in [editor]
-# ff file [num] --editor [editor]
+        return results
 
-# find and cd into directory to which the file belongs
-# ff file [num] -cd
-
-# find and do an svn diff
-# ff file [num] --svn-diff
-
-parser = argparse.ArgumentParser()
-parser.add_argument('searchstring')
-parser.add_argument('index', nargs='?', type=int)
-
-group = parser.add_mutually_exclusive_group()
-group.add_argument('--open', action='store')
-group.add_argument('--cd', action='store_true')
-group.add_argument('--svn-diff', action='store_true')
-group.add_argument('--hg-diff', action='store_true')
-
-args = parser.parse_args()
-#print "=====================", args
-
-action = getAction(args)
-index = getIndex(args, action)
-openCmd = getOpenCmd(args)
-searchstring = args.searchstring
-
-
-#print "exit"
-#sys.exit()
-
-
-#print 'searchstring =', searchstring
-print 'action = ', action
-#print 'index =', index
-print 'openCmd =', openCmd #, openCmd.__class__.__name__
+    def matches(self, searchstring, item):
+        return re.search(searchstring, item, re.IGNORECASE)
 
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('searchstring')
+    parser.add_argument('basedir', nargs='?')
+    return parser.parse_args()
 
-execute(os.curdir, searchstring, action, index, editor=openCmd)
+args = parse_args()
+basedir = os.curdir
+if args.basedir is not None:
+    basedir = args.basedir
+
+pie_find = PieFind()
+path_list = sorted(pie_find.find_files(basedir, args.searchstring))
+
+ui = PieUI(path_list)
+ui.start()
